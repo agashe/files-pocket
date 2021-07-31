f162e605cfb77da5e9687a3f82e26cb1ae807691
Mohamed Yousef <engineer.mohamed.yossef@gmail.com>
2021-07-31 09:22:24 +0200

    working on routes

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+/** Auth Routes **/
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('auth')->group(function () {
+    /** Home Routes **/
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+
+    /** Folders Routes **/
+    Route::resource('folders', FolderController::class)->except(['create', 'edit', 'update']);
+
+    /** Files Routes **/
+    Route::resource('files', FileController::class)->only(['store', 'destroy']);
+});
